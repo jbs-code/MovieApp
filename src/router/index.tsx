7f26c9620cd5209e79a5d9a63fcf076fb3b0d848
“jaimejb” <jaimejb.code@gmail.com>
2026-08-16 08:20:14 -0600

    Router: createBrowserRouter changed to createHashRouter

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,10 @@
-import { Navigate, createBrowserRouter } from "react-router";
+import { Navigate, createHashRouter } from "react-router";
 
 import { MainLayout } from "../layouts/MainLayout";
 import { MovieDetails } from "../movie-details";
 import MoviesApp from "../MoviesApp";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/movies",
     Component: MainLayout,
